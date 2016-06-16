a9c6d67bdcbebf0815fa1559cb9599c6d7fbcd1f
ByungDae Sohn <miconblog@gmail.com>
2016-06-16 17:00:35 -0400

    기존에 사용하고 있던 샘플 마크 다운과 미리보기 샘플 추가

--- src/RlibroEditor.jsx
+++ src/RlibroEditor.jsx
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux'
+import { render } from '../src/markdown'
 import _ from 'lodash';
 
 export default class RlibroEditor extends Component {
@@ -24,20 +25,39 @@ export default class RlibroEditor extends Component {
   componentDidUpdate(prevProps, prevState) {
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  // }
 
   componentWillUnmount() {
   }
 
   render() {
+    const { html } = this.state;
 
-    return (
-      <div className="rlibo-editor">
-        에디터 테스트
+    return <div className="r-editor">
+      <div className="r-header">
+        <p className="r-markdown-help">
+          <a href="https://guides.github.com/features/mastering-markdown/" target="_blank" data-ga-click="Markdown Toolbar, click, help">
+            <svg aria-hidden="true" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16">
+              <path d="M14.85 3H1.15C0.52 3 0 3.52 0 4.15v7.69C0 12.48 0.52 13 1.15 13h13.69c0.64 0 1.15-0.52 1.15-1.15V4.15C16 3.52 15.48 3 14.85 3zM9 11L7 11V8l-1.5 1.92L4 8v3H2V5h2l1.5 2 1.5-2 2 0V11zM11.99 11.5L9.5 8h1.5V5h2v3h1.5L11.99 11.5z"></path>
+            </svg>
+            Styling with Markdown is supported
+          </a>
+        </p>
       </div>
-    );
 
+      <div className="r-preview" dangerouslySetInnerHTML={{__html: html}}></div>
+
+      <div className="r-split-bar" />
+
+      <textarea ref="textarea" onChange={this.handleChange} />
+    </div>
   }
+
+  handleChange =(e)=>{
+    this.setState({
+      html: render(e.target.value) 
+    })
+  };
 
 };
---
+++ src/markdown.js
@@ -0,0 +1,39 @@
+var marked = require('marked');
+var _ = require('lodash');
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: true
+});
+
+
+function makeMarkerLink(text, id, placeIds, places, routingNoteId, routingPlaceId) {
+
+  // 실제로 등록된 장소만 링크를 걸어준다. 
+  _.each(placeIds, function(pid){
+
+    var p = _.find(places, function(pl){ return pl.id === pid});
+    
+    if( pid !== routingPlaceId && p ) {
+      let regx = RegExp(`\\[([^\\[\\]]*)\\]\\[${pid}\\]`,'g');
+      text = text.replace(regx, `<a target="_blank" href="/notes/${id}/places/${pid}"><i class="fa icon-pin"></i>$1</a>`);
+    }
+  });
+
+  // 장소ID를 잘못 입력하면 그냥 없애준다. 
+  return text.replace(/\[([^\[\]]*)\]\[([a-zA-Z0-9]{10})\]/g, '$1')
+}
+
+export function render (text, noteId, placeIds, places, routingNoteId, routingPlaceId) {
+
+  text = marked(text);
+  text = makeMarkerLink(text, noteId, placeIds, places, routingNoteId, routingPlaceId);
+    
+  return text;
+}
